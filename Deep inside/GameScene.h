f7431cdfdfbdaa5c9d46aa566f49110c21ce6cd1
SKOLZ <gabriel.zanzotti@wolox.com.ar>
2016-05-11 16:25:28 -0300

    Initial Commit

---
+++ Deep inside/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Deep inside
+//
+
+//  Copyright (c) 2016 White Stone. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
